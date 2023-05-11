8d1420da0784c83f5183c12fe81afe494f32069a
chein5 <mati25quezada@gmail.com>
2023-05-11 16:18:24 -0400

    Se creo un ejemplo de una parte del nivel
    Se añadió un pulse para mostrar como tienen que ser las actividades.

--- Index.php
+++ Index.php
@@ -1,9 +1,17 @@
     <!DOCTYPE html>
     <html>
     <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World</p>'; ?>
+        
+
+        <a class="btn-floating btn-large pulse"><i class="material-icons">cloud</i></a>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+        <?php echo '<p>Presione con el mouse</p>'; ?>
     </body>
     </html>
