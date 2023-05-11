f63cf33006cab3c06f79f2ca397d76ceb40ed1fb
chein5 <mati25quezada@gmail.com>
2023-05-11 15:06:00 -0400

    Actualización del control de profesor.
    se actualizo el controlador.

--- Controlador/Control_profe.php
+++ Controlador/Control_profe.php
@@ -1,6 +1,17 @@
 <?php
     class CRUD_profe{
-        
+
+        /**
+         * @param $rut_profesor esta sera la PK de la tabla profesor
+         * @param $nombre_profesor nombre del profesor en cuestion
+         * @param $apellido_profesor apellido del profesor en cuestion
+         * @param $estado_profesor estado del profesor dentro del sistema
+         */
+         
+        public function crear_profesor($rut_profesor,$nombre_profesor,$apellido_profesor,$estado_profesor){
+            
+
+        }
     }
 
 ?>
