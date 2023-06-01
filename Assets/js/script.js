1b5fa908054af286e0c2cbef39bc210156342740
Ryoto01 <mario.ignacio.mora@gmail.com>
2023-06-01 06:03:54 -0400

    Entrega de pagina beta kiwi
    se han implementado los siguientes archivos junto con la eliminacion de otros:
    -se añadio la carpeta assets la cual contiene todos los recursos necesarios para el buen funcionamiento de la pagina.
    -se añadio el archivo index, este contiene la pagina de inicio del proyecto siendo esta una landpage.
    -se añadio el archivo clase, este contiene una simulacion de lo que se espera sea el funcionamiento final de las clases en el proyecto kiwi.
    -se elminaron los archivos: index, recursos siendo reemplazados.

---
+++ Assets/js/script.js
@@ -0,0 +1,37 @@
+window.onscroll = function () {
+
+    scroll = document.documentElement.scrollTop;
+
+    header = document.getElementById("header");
+
+    if (scroll > 30) {
+        header.classList.add('nav_mod');
+    } else if (scroll < 30) {
+        header.classList.remove('nav_mod');
+    }
+
+}
+
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+
+menu = document.getElementById("header");
+nav = document.getElementById("nav");
+body = document.getElementById("container_all");
+
+function mostrar_menu() {
+
+    menu.classList.toggle('move_content');
+    nav.classList.toggle('move_nav');
+    body.classList.toggle('move_content');
+}
+
+
+window.addEventListener("resize", function () {
+
+    if (window.innerWidth > 1024) {
+        menu.classList.remove('move_content');
+        body.classList.remove('move_content');
+        nav.classList.remove('move_nav');
+    }
+
+})
